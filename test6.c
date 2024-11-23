2c1af4296f4515c41f7c77ee122898808ec90b3a
Sam Russell <sam.h.russell@gmail.com>
2024-11-23 16:48:41 +0100

    tidied up code and changed to 32-bit constants

--- test6.c
+++ test6.c
@@ -10,14 +10,14 @@ int reduce32(__m128i in) {
     mu = _mm_set_epi64x(0, 0x1F7011641);
     a = _mm_and_si128(in, _mm_set_epi64x(0, 0xffffffff));
     a = _mm_clmulepi64_si128(a, mu, 0x00);
-    printf("multiply by mu %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(a, 8)), _mm_cvtsi128_si64(a));
+    //printf("multiply by mu %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(a, 8)), _mm_cvtsi128_si64(a));
     a = _mm_and_si128(a, _mm_set_epi64x(0, 0xffffffff));
     // multiply by p
     p = _mm_set_epi64x(0, 0x1DB710641);
     a = _mm_clmulepi64_si128(a, p, 0x00);
-    printf("multiply by p %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(a, 8)), _mm_cvtsi128_si64(a));
+    //printf("multiply by p %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(a, 8)), _mm_cvtsi128_si64(a));
     a = _mm_xor_si128(in, a);
-    printf("xor with input %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(a, 8)), _mm_cvtsi128_si64(a));
+    //printf("xor with input %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(a, 8)), _mm_cvtsi128_si64(a));
     a = _mm_srli_si128(a, 4);
     return _mm_cvtsi128_si32(a);
 }
@@ -26,7 +26,7 @@ __m128i reduce64(__m128i in_high, __m128i in_low) {
     // split up into 32-bit chunks
     __m128i a;
     // multiply by 64-bit shift
-    __m128i shift64 = _mm_set_epi64x(0, 0x163cd6124);
+    __m128i shift64 = _mm_set_epi64x(0, 0xB8BC6765);
     a = _mm_clmulepi64_si128(shift64, in_high, 0x00);
     a = _mm_xor_si128(a, in_low);
     return a;
@@ -50,7 +50,7 @@ int main (int argc, char* argv[]) {
     int bytes_remaining = len;
     __m128i a;
 
-    __m128i shift544_shift480 = _mm_set_epi64x(0x1c6e41596, 0x154442bd4);
+    __m128i shift544_shift480 = _mm_set_epi64x(0x1D9513D7, 0x8F352D95);
 
     while (bytes_remaining >= 128) {
         // fold by 4x128-bits
@@ -91,14 +91,14 @@ int main (int argc, char* argv[]) {
         data += 64;
     }
 
-    __m128i shift160_shift96 = _mm_set_epi64x(0x0ccaa009e, 0x1751997d0);
+    __m128i shift160_shift96 = _mm_set_epi64x(0xCCAA009E, 0xAE689191);
 
     while (bytes_remaining >= 32) {
         // fold by 128-bits
         __m128i in1 = _mm_loadu_si128(data);
         __m128i in2 = _mm_loadu_si128(data + 16);
-        printf("in1 %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(in1, 8)), _mm_cvtsi128_si64(in1));
-        printf("in2 %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(in2, 8)), _mm_cvtsi128_si64(in2));
+        //printf("in1 %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(in1, 8)), _mm_cvtsi128_si64(in1));
+        //printf("in2 %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(in2, 8)), _mm_cvtsi128_si64(in2));
 
         // now we fold in1 onto in2
         __m128i fold_high = _mm_clmulepi64_si128(in1, shift160_shift96, 0x11);
@@ -130,8 +130,8 @@ int main (int argc, char* argv[]) {
         memcpy(in256 + (32 - bytes_remaining), data, bytes_remaining);
         __m128i in1 = _mm_loadu_si128((void*)in256);
         __m128i in2 = _mm_loadu_si128((void*)in256 + 16);
-        printf("in1 %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(in1, 8)), _mm_cvtsi128_si64(in1));
-        printf("in2 %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(in2, 8)), _mm_cvtsi128_si64(in2));
+        //printf("in1 %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(in1, 8)), _mm_cvtsi128_si64(in1));
+        //printf("in2 %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(in2, 8)), _mm_cvtsi128_si64(in2));
 
         // now we fold in1 onto in2
         __m128i shift160_shift96 = _mm_set_epi64x(0x0ccaa009e, 0x1751997d0);
@@ -139,7 +139,7 @@ int main (int argc, char* argv[]) {
         __m128i fold_low = _mm_clmulepi64_si128(in1, shift160_shift96, 0x00);
         in2 = _mm_xor_si128(in2, fold_high);
         a = _mm_xor_si128(in2, fold_low);
-        printf("folded in2 %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(a, 8)), _mm_cvtsi128_si64(a));
+        //printf("folded in2 %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(a, 8)), _mm_cvtsi128_si64(a));
     }
     else {
         a = _mm_loadu_si128(data);
@@ -150,9 +150,9 @@ int main (int argc, char* argv[]) {
     __m128i in64 = _mm_and_si128(a, _mm_set_epi64x(0, 0xffffffffffffffff));
     __m128i in_high = _mm_and_si128(_mm_srli_si128(a, 8), _mm_set_epi64x(0, 0xffffffff));
     __m128i in_low = _mm_and_si128(_mm_srli_si128(a, 12), _mm_set_epi64x(0, 0xffffffff));
-    printf("in64 %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(in64, 8)), _mm_cvtsi128_si64(in64));
-    printf("in_high %016llx\n", _mm_cvtsi128_si64(in_high));
-    printf("in_low %016llx\n", _mm_cvtsi128_si64(in_low));
+    //printf("in64 %016llx%016llx\n", _mm_cvtsi128_si64(_mm_srli_si128(in64, 8)), _mm_cvtsi128_si64(in64));
+    //printf("in_high %016llx\n", _mm_cvtsi128_si64(in_high));
+    //printf("in_low %016llx\n", _mm_cvtsi128_si64(in_low));
     a = reduce128(in64);
     // first 32 bits go on in_high
     in_high = _mm_xor_si128(in_high, _mm_and_si128(a, _mm_set_epi64x(0, 0xffffffff)));
